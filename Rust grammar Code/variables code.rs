6af592ebf7ba52f88a37dfa5818629d063d2dec1
chani23 <145738794+chani23@users.noreply.github.com>
2023-11-19 16:51:15 +0900

    Add files via upload

---
+++ Rust grammar Code/variables code.rs
@@ -0,0 +1,101 @@
+//fn main() {
+    //let mut x = 5;
+    //println!("The value of x is: {}", x);
+    //x = 6;
+    //println!("The value of x is: {}", x)
+
+//} 
+
+//fn main() {
+    //let x = 5;
+
+    //let x = x + 1 ;
+
+    //let x = x * 2 ;
+    //println!("The value of x is: {}", x)
+//}
+
+/*fn main() {
+    let x= 2.0;
+
+    let y: f32 = 3.0;
+    print!("나올 숫자는: {} ",x );
+    print!("나올 숫자는: {} ",y );
+
+}
+*/
+/*fn main(){
+    
+    let sum = 5.7 + 10.7;
+
+    let difference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+
+    let remainder = 43 % 5;
+
+    println!("{}",sum);
+    println!("{}",difference);
+    println!("{}",product);
+    println!("{}",quotient);
+    println!("{}",remainder);
+}*/
+/*fn main() {
+    let t = true ;
+
+    let f : bool = false;
+
+    println!("{}", t);
+    println!("{}",f );
+}*/
+/*fn main(){
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("{}", c);
+    println!("{}", z);
+    println!("{}", heart_eyed_cat);
+}*/
+/*fn main(){
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("the value of x is: {}", x);
+    println!("the value of y is: {}", y);
+    println!("the value of z is: {}", z);
+}*/
+
+/*fn main() {
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("first index of x is {}",five_hundred);
+    println!("second index of x is {}",six_point_four);
+    println!("third index of x is {}",one);
+}*/
+
+/*fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+
+    println!("first of a is {}",first);
+    println!("second of a is {}", second);
+}*/
+
+//인덱스에 벗어나는 범위일때 오류가 생김
+/*fn main() {
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", element);
+}*/
